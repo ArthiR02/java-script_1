4f5415be9b5e153c94b84f66627f5fb03def0e05
ARTHI R <129277654+ArthiR02@users.noreply.github.com>
2023-07-11 18:31:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+ function add(){
+    var x = parseInt(document.getElementById("n1").value);
+    var y = parseInt(document.getElementById("n2").value);;
+    var z = x + y;
+ document.getElementById("add").innerHTML = z;
+ }
+// function sub(){
+// var x = 5;
+// var y = 2;
+// var z = x - y;
+// document.getElementById("sub").innerHTML = z;
+// }
+// function mul(){
+// var x = 4;
+// var y = 21;
+// var z = x * y;
+// document.getElementById("mul").innerHTML = z;
+// }
+function sub(){
+    var x = document.getElementById("n1").value;
+    var y = document.getElementById("n2").value;
+    var z = x - y;
+    document.getElementById("sub").innerHTML = z;
+}
+function mul(){
+    var x = document.getElementById("n1").value;
+    var y = document.getElementById("n2").value;
+    var z = x * y;
+    document.getElementById("mul").innerHTML = z;
+}
+function div(){
+    var x = document.getElementById("n1").value;
+    var y = document.getElementById("n2").value;
+    var z = x / y;
+    document.getElementById("div").innerHTML = z;
+}
+
